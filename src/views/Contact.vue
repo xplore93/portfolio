7c386617b6ef3bc2c6b67fdcb7eaa6bc939ff543
Eugeniu Stina <xplore93@gmail.com>
2020-09-25 19:03:59 +0200

    Feat: Contact form

---
+++ src/views/Contact.vue
@@ -0,0 +1,27 @@
+<template>
+<p>Work in progress</p>
+<form action="https://formspree.io/xdopoqbr" method="POST">
+    <label>
+        Su correo:
+        <input type="text" name="_replyto">
+    </label>
+    <label>
+        Su mensaje:
+        <textarea name="message"></textarea>
+    </label>
+
+    <!-- your other form fields go here -->
+
+    <button type="submit">Enviar</button>
+</form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Resume.vue
@@ -0,0 +1,13 @@
+<template>
+<p>Work in progress</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
