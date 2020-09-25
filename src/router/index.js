a616ab420a76eb52de047fce38002bd896960e85
Eugeniu Stina <xplore93@gmail.com>
2020-09-25 19:06:45 +0200

    Feat: Added new routes for Resume and Contact

--- src/router/index.js
+++ src/router/index.js
@@ -17,13 +17,22 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/Projects")
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/resume",
+    name: "Resume",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/Resume")
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Contact")
   }
 ];
 
