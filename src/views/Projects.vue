b45f2e11f4dc0544a6e25bf7cf2b1d84d09b5bdf
Eugeniu Stina <xplore93@gmail.com>
2020-09-25 18:57:39 +0200

    Fix: Set link for 42 projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="row mx-auto">
-    <Group title="Proyectos de 42Madrid" text="Los Proyectos que he realizado durante el curso en 42Madrid" url="/" img="42.png" />
-    <Group title="Proyectos personales" text="Los Proyectos que he realizado individualmente o en equipo" url="/" img="self.jpeg" />
+    <Group title="Proyectos de 42Madrid" text="Los Proyectos que he realizado durante el curso en 42Madrid" url="https://github.com/xplore93/42Madrid" img="42.png" />
+    <Group title="Proyectos personales" text="Los Proyectos que he realizado individualmente o en equipo" url="#" img="self.jpeg" />
 </div>
 </template>
 
